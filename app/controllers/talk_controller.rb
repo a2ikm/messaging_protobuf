2ad64343d4f31c7c18dee853422f278e8e551faf
Masato Ikeda <masato.ikeda@gmail.com>
2018-04-03 02:17:32 +0900

    Implement concepts

--- app/controllers/talk_controller.rb
+++ app/controllers/talk_controller.rb
@@ -1,4 +1,24 @@
 class TalkController < ApplicationController
-  def start
+  class <<self
+    Messaging = Struct.new(:request, :response, :options)
+
+    def messaging(args = {})
+      (req, res) = args.shift || []
+      @last_messaging = Messaging.new(req, res, args)
+    end
+
+    def method_added(name)
+      if @last_messaging
+        @messagings ||= {}
+        @messagings[name] = @last_messaging
+        @last_messaging = nil
+      end
+    end
+  end
+
+  messaging Hello => Hello
+  def start(hello)
+    Rails.logger.info "Hello from #{hello.from}"
+    reply = Hello.new(from: `hostname`.chomp)
   end
 end
